6da2a12eb5565af525b57572f222a20126bfa2f6
MoinHaiderQadir <moinhaiderqadir@gmail.com>
2024-12-12 18:28:21 +0500

    up

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default {
-  build: {
-    outDir: 'build' // Ensure this matches the gh-pages `-d` option
-  }
-};
+
+export default defineConfig({
+  plugins: [react()],
+})
